ab567bc271b65db313f3a04482b9699a5ebe53c7
AmyNithin <amruthapb2@gmail.com>
2017-06-08 23:11:04 +1200

    commited

--- Xero_Project/src/test_setup.java
+++ Xero_Project/src/test_setup.java
@@ -1,11 +1,11 @@
-import java.util.Random;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class test_setup extends Random_check {
+public class test_setup {
 	
 	public static void main(String[] args)throws Exception{
 		
@@ -18,24 +18,15 @@ public class test_setup extends Random_check {
 	
 		wd.findElement(By.xpath("//a[@class='btn btn-tertiary-alt global-ceiling-bar-btn']")).click();
 		
-		wd.findElement(By.xpath("//input[@id='email']")).clear();
-		
-		//Random_check R_e=new Random_check();
-	    
-	    
-	    wd.findElement(By.xpath("//input[@id='email']")).sendKeys(Random_check.email());
-			
+		wd.findElement(By.xpath("//input[@id='email']")).clear(); 
+	    wd.findElement(By.xpath("//input[@id='email']")).sendKeys("amruthapb2@gmail.com");	
 		wd.findElement(By.xpath("//input[@id='password']")).clear();
-		Random_check R_pwd=new Random_check();
-		//wd.findElement(By.xpath("//input[@id='password']")).sendKeys(R_pwd);
-		R_pwd.password();
-		
-		
+	    wd.findElement(By.xpath("//input[@id='password']")).sendKeys("wellington1");
 		wd.findElement(By.xpath("//button[@id='submitButton]']")).click();
 		
 		wd.findElement(By.xpath("//a[@id='Accounts']")).click();
-		//Select dropdown=new Select(wd.findElement(By.xpath("//a[@id='Accounts']")));
-		//dropdown.selectByVisibleText("Bank Accounts");
+		Select dropdown=new Select(wd.findElement(By.xpath("//a[@id='Accounts']")));
+		dropdown.selectByVisibleText("Bank Accounts");
 		
 		wd.findElement(By.xpath("//span[@class='text']")).click();
 		wd.findElement(By.xpath("//input[@id='xui-searchfield-1018-inputEl']")).clear();
@@ -43,9 +34,7 @@ public class test_setup extends Random_check {
 		dropdown1.selectByVisibleText("ANZ (NZ)");
 		
 		wd.findElement(By.xpath("//input[contains(@id,'accountname')]")).clear();
-		
-		Random_check R_name=new Random_check();
-		R_name.accountname();
+		wd.findElement(By.xpath("//input[contains(@id,'accountname')]")).sendKeys("amy");
 		
 		wd.findElement(By.xpath("//input[contains(@id,'accounttype')]")).click();
 		Select dropdown2=new Select(wd.findElement(By.xpath("//input[contains(@id,'accounttype')]")));
@@ -54,8 +43,7 @@ public class test_setup extends Random_check {
 		wd.findElement(By.xpath("//input[contains(@id,'accountnumber')]"));
 		
 		wd.findElement(By.xpath("//input[@id='password']")).clear();
-		Random_check acc_no=new Random_check();
-		acc_no.accountno();
+		wd.findElement(By.xpath("//input[@id='password']")).sendKeys("123789043");
 		wd.findElement(By.xpath("//input[contains(@id,'currencyCombo-1069-inputEl')]")).click();
 		wd.findElement(By.xpath("//span[contains(id,'common-button')]")).click();
 		
