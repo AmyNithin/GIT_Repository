3b44c7383c9549297111f9c2e582cca00ff00021
AmyNithin <amruthapb2@gmail.com>
2017-08-17 12:36:04 +1200

    Commit
    Commit

--- Xero_Project/src/test_setup.java
+++ Xero_Project/src/test_setup.java
@@ -1,15 +1,4 @@
-
-
-import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class test_setup {
 	
@@ -24,6 +13,7 @@ public class test_setup {
 		
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='btn btn-tertiary-alt global-ceiling-bar-btn']")));
 
+		
 	
 		wd.findElement(By.xpath("//a[@class='btn btn-tertiary-alt global-ceiling-bar-btn']")).click();
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='email']")));
@@ -35,24 +25,9 @@ public class test_setup {
 	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='submitButton']")));
 		wd.findElement(By.xpath("//button[@id='submitButton']")).click();
 		
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@id='Accounts']")));
-		wd.findElement(By.xpath("//a[@id='Accounts']")).click();
-		
-		wd.findElement(By.linkText("Bank Accounts")).click();
-		
-		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@data-type='menu-focus']")));
-		String a=((WebElement) wd.findElements(By.tagName("li"))).getText();
-		System.out.println("text is"+a);
-		//List<WebElement> links=wd.findElements(By.tagName("li"));
-		//for(WebElement link:links){
-		//	if(link.getText().equals("Bank Accounts"))
-		//		link.click();
-		//	break;
-		
-	//	}
 		
 		
-		
+
 		wd.findElement(By.xpath("//span[@class='text']")).click();
 		wd.findElement(By.xpath("//input[@id='xui-searchfield-1018-inputEl']")).clear();
 		Select dropdown1=new Select(wd.findElement(By.xpath("//a[@id='Accounts']")));
